e9ba3095b029bd66b2b9f8dc6d69c4fd7cba3879
Ge0m <35548187+Ge0m@users.noreply.github.com>
2025-10-02 20:39:48 +0000

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,13 @@ module.exports = {
       },
     },
   },
+  safelist: [
+    'from-orange-600', 'to-orange-700', 'from-orange-300', 'from-orange-400', 'via-orange-400',
+    'from-green-600', 'to-green-700', 'from-red-600', 'to-red-700',
+    'text-orange-300', 'text-orange-400', 'text-orange-600',
+    'border-orange-400', 'border-orange-500',
+    'bg-gradient-to-br', 'bg-gradient-to-r', 'bg-clip-text', 'bg-transparent',
+    'shadow-xl', 'shadow-2xl', 'shadow-lg', 'shadow-md', 'hover:shadow-xl', 'hover:shadow-lg'
+  ],
   plugins: [],
 };
